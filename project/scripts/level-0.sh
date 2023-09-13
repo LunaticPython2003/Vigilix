1b7bad7980add8e5fa25bcbecd50acbe766c8f06
Madhurjya Dasgupta <madhurjya2003@icloud.com>
2023-09-13 14:39:34 +0530

    Completed level 0

--- project/scripts/level-0.sh
+++ project/scripts/level-0.sh
@@ -3,10 +3,6 @@
 # Ensure superuser access
 sudo su
 
-# Turn off the USB interfaces
-# echo "blacklist uas" >> /etc/modprobe.d/blacklist.conf
-# echo "blacklist usb-storage" >> /etc/modprobe.d/blacklist.conf
-
 # Turn off Bluetooth
 rmmod --force btusb
 # To enable - modprobe btusb
@@ -15,3 +11,10 @@ rmmod --force btusb
 chmod 0000 $(whereis apt)
 #To re-enable -> chmod 7777 $(whereis apt)
 
+
+# Turn off the USB interfaces
+echo 'ACTION=="add", SUBSYSTEMS=="usb", RUN+="/bin/sh -c '\''echo 0 >/sys$DEVPATH/authorized'\''"' >> /etc/udev/rules.d/99-usb-restrict.rules
+udevadm control --reload-rules
+
+# Deactivate SSH
+systemctl stop ssh
