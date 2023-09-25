4d4cdebaa74c738175a92bd9bcdda6451717c3a8
Madhurjya Dasgupta <madhurjya2003@icloud.com>
2023-09-26 04:11:44 +0530

    Bug fixes

--- app/scripts/level-1-reset.sh
+++ app/scripts/level-1-reset.sh
@@ -1,8 +1,9 @@
 modprobe btusb # Renables bluetooth
-systemctl start ssh # Restarts the ssh
 gsettings set org.gnome.system.location enabled true # Enables location
 
 # Re enable USB
 rm -rf /etc/udev/rules.d/99-usb-restrict.rules
 echo 'ACTION=="add", SUBSYSTEMS=="usb", RUN+="/bin/sh -c '\''echo 1 >/sys$DEVPATH/authorized'\''"' >> /etc/udev/rules.d/99-usb-restrict.rules
 udevadm control --reload-rules
+
+service NetworkManager start
--- app/scripts/level-1.sh
+++ app/scripts/level-1.sh
@@ -3,11 +3,10 @@
 # Turn off Bluetooth
 rmmod --force btusb
 
-# Ability to install apps
-chmod 0000 $(whereis apt)
-
 # Turn off the USB interfaces
 echo 'ACTION=="add", SUBSYSTEMS=="usb", RUN+="/bin/sh -c '\''echo 0 >/sys$DEVPATH/authorized'\''"' >> /etc/udev/rules.d/99-usb-restrict.rules
 udevadm control --reload-rules
 
 gsettings set org.gnome.system.location enabled false ## Disables location
+
+service NetworkManager stop # Disables Wifi
